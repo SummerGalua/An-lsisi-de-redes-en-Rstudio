b2f41fc7110f18e14389c47259fb13bc7bda7f1f
SummerGalua <69013786+SummerGalua@users.noreply.github.com>
2021-02-11 08:56:05 -0600

    Update Conectividad y Distancia media de Conexión.R

--- Conectividad y Distancia media de Conexión.R
+++ Conectividad y Distancia media de Conexión.R
@@ -1,21 +1,22 @@
 #Generamos el grafo 
-datos <- data.frame(readxl::read_excel("Vías_Ferrocarriles.xlsx"))
-SN <-datos[1:2]
-
-#Original 
+datos <- data.frame(readxl::read_excel("C:/PATH", sheet = "Nombre de la página de excel"))
+SN <-data.frame(datos$Origen.entidad,datos$Destino.entidad)
 g <- igraph::graph.edgelist(as.matrix(SN), directed = FALSE) 
 
 #Graficamos el Grafo
+networkD3::simpleNetwork(SN[],fontSize = 10, linkDistance = 1,charge = -4)
 
-networkD3::simpleNetwork(SN[],fontSize = 20, linkDistance = 1)
-
-#Cálculo de conectividad (Promedio de aristas por nodo)
+#Cálculo de conectividad
 k <- mean(igraph::degree(g))
-
-#Cálculo de la distancia media de conexión
-
-igraph::E(g)$weight<- datos$Distancia
+igraph::E(g)$weight<- datos$Distancia..km.
 distancia_media_de_conexión <- sum(igraph::distances(g))/ (2 * sum(seq(1,dim(igraph::distances(g))[1]-1,1)))
+
+#Identificación del arco más largo
+maxdist<-max(igraph::distances(g))
+sp <- igraph::shortest.paths(g, v = "Nodo inicial", to = "Nuevo Laredo")
+gsp <- igraph::get.shortest.paths(g, from = "Nodo inicial",to = "Nodo final")
+igraph::V(g)[gsp$vpath[[1]]]
+
 
 
 
